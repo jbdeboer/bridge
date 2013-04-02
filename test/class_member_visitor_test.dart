8d03e3affc8b1151fbeb23aec4e83dbb06c6a3e7
James deBoer <james@huronbox.com>
2013-04-02 10:00:28 -0700

    cleaned up ExpressionVisitorTest, ClassMemberVisitorTest

--- test/class_member_visitor_test.dart
+++ test/class_member_visitor_test.dart
@@ -1,46 +1,20 @@
-import 'package:analyzer_experimental/src/generated/java_core.dart';
-import 'package:analyzer_experimental/src/generated/java_engine.dart';
-import 'package:analyzer_experimental/src/generated/java_junit.dart';
-
-
 import 'package:unittest/unittest.dart';
-import 'package:analyzer_experimental/src/generated/ast.dart';
-import 'package:analyzer_experimental/src/generated/scanner.dart';
-import 'package:analyzer_experimental/src/generated/error.dart';
-import 'package:analyzer_experimental/src/generated/parser.dart';
-
-import '../lib/src/jsast/js.dart' as js;
 
 import '../lib/src/class_member_visitor.dart';
-import '../lib/src/listeners.dart';
 import '../lib/src/parse.dart';
-import '../lib/src/stub_visitor.dart';
 import '../lib/src/utils.dart';
 
 
-stringBridge(String dart) {
-  PrintStringWriter psw = new PrintStringWriter();
-  ASTNode n = parseText(dart);
-  var visitor = new ClassMemberVisitor(new StubVisitor());
-
-  for (var s in n.accept(visitor)) {
-      psw.print(js.prettyPrint(s).getText());
-    }
-  return psw.toString();
-}
-
-class BVT {
- static expectParse(String dart, String jsCode) {
-    expect(stringBridge(dart), equals(jsCode));
- }
+expectClass(String dart, String jsCode) {
+  expect(stringBridge(dart, (x) => new ClassMemberVisitor(x)),
+         equals(dedent(jsCode)));
 }
 
 main() {
   test('should parse a simple class', () {
-    BVT.expectParse(
-        'class E { String x; }',
+    expectClass('class E { String x; }',
         // JS.
-        dedent("""
+        """
            /**
             * @constructor
             */
@@ -49,18 +23,18 @@ main() {
            /** @type {string} */
            E.prototype.x;
            """
-        ));
+        );
   });
 
 
   test('should parse a class with a constructor', () {
-    BVT.expectParse(dedent("""
+    expectClass("""
         class C {
           String x;
           C(this.x);
-        }"""),
+        }""",
     // JS.
-    dedent("""
+    """
         /\**
          * @constructor
          */
@@ -69,16 +43,16 @@ main() {
         }
         /\** @type {string} */
         C.prototype.x;
-        """));
+        """);
   });
 
   test('should parse a class with a method', () {
-    BVT.expectParse(dedent("""
+    expectClass("""
         class C {
           String method() { return "a"; };
-        }"""),
+        }""",
     // JS.
-    dedent("""
+    """
         /\**
          * @constructor
          */
@@ -90,6 +64,6 @@ main() {
         C.prototype.method = function() {
           // STUB BLOCK
         };
-         """));
+         """);
   });
 }
--- test/expression_visitor_test.dart
+++ test/expression_visitor_test.dart
@@ -1,201 +1,201 @@
-import 'package:analyzer_experimental/src/generated/java_core.dart';
-import 'package:analyzer_experimental/src/generated/java_engine.dart';
-import 'package:analyzer_experimental/src/generated/java_junit.dart';
-
-
 import 'package:unittest/unittest.dart';
 import 'package:analyzer_experimental/src/generated/ast.dart';
-import 'package:analyzer_experimental/src/generated/scanner.dart';
-import 'package:analyzer_experimental/src/generated/error.dart';
-import 'package:analyzer_experimental/src/generated/parser.dart';
-
-import '../lib/src/listeners.dart';
-import '../lib/src/parse.dart';
-import '../lib/src/utils.dart';
-import '../lib/src/debug.dart';
 
 import '../lib/src/expression_visitor.dart';
-import '../lib/src/stub_visitor.dart';
-import '../lib/src/jsast/js.dart' as js;
+import '../lib/src/parse.dart';
 
 
-stringBridge(String dart) {
-  PrintStringWriter psw = new PrintStringWriter();
-  CompilationUnit n = parseText("var x = $dart");
-  Expression expr =
-    n.sortedDirectivesAndDeclarations[0].
+queryExpression(node) =>
+  node.sortedDirectivesAndDeclarations[0].
         variables.variables[0].initializer;
 
-  var visitor = new ExpressionVisitor(new StubVisitor());
-
-  for (var s in expr.accept(visitor)) {
-    psw.print(js.prettyPrint(s).getText());
-  }
-  return psw.toString();
-}
-
-class BVT {
-  static expectParse(String dart, String jsCode) {
-    expect(stringBridge(dart), equals(jsCode));
-  }
+expectExpr(String dart, String jsCode) {
+  expect(stringBridge("var x = $dart",
+      (x) => new ExpressionVisitor(x),
+      queryExpression),
+    equals(jsCode));
 }
 
 main() {
-
   // Basic literals.
-
   test('should parse string literals', () {
-    BVT.expectParse('"A"', '"A"');
+    expectExpr('"A"', '"A"');
   });
 
   test('should parse integers', () {
-    BVT.expectParse('4', '4');
+    expectExpr('4', '4');
   });
 
   test('should parse doubles', () {
-    BVT.expectParse('4.3', '4.3');
+    expectExpr('4.3', '4.3');
   });
 
   test('should parse booleans', () {
-    BVT.expectParse('true', 'true');
+    expectExpr('true', 'true');
   });
 
   test('should parse null literals', () {
-    BVT.expectParse('null', 'null');
+    expectExpr('null', 'null');
   });
+
+
 
   // Typed literals.
-
   test('should parse list literals', () {
-    BVT.expectParse('[]', '[]');
+    expectExpr('[]', '[]');
   });
 
   test('should parse list literals', () {
-    BVT.expectParse('[1,2]', '[1, 2]');
+    expectExpr('[1,2]', '[1, 2]');
   });
 
   test('should parse map literals', () {
-    BVT.expectParse('{}', '{}');
+    expectExpr('{}', '{}');
   });
 
   test('should parse map literals with properties', () {
-    BVT.expectParse('{"a":1, "b":2}', '{a: 1, b: 2}');
+    expectExpr('{"a":1, "b":2}', '{a: 1, b: 2}');
   });
 
-  // String literals.
 
+
+  // String literals.
   test('should parse adjacent strings', () {
-    BVT.expectParse('"a" "b"', '"a" + "b"');
+    expectExpr('"a" "b"', '"a" + "b"');
   });
 
   test('should parse string interpolation', () {
-    BVT.expectParse('"a\$\{1\}b', 'STUB STRING INTERPOLATION');
+    expectExpr('"a\$\{1\}b', 'STUB STRING INTERPOLATION');
   });
 
-  // Identifiers
 
+
+  // Identifiers
   test('should punt on library identifiers', () {
-    BVT.expectParse('lib.name', 'stubIDENTIFIER_lib.name');
+    expectExpr('lib.name', 'stubIDENTIFIER_lib.name');
   });
 
   test('should punt on prefix identifiers', () {
-    BVT.expectParse('prefix.name', 'stubIDENTIFIER_prefix.name');
+    expectExpr('prefix.name', 'stubIDENTIFIER_prefix.name');
   });
 
   test('should punt on simple identifiers', () {
-    BVT.expectParse('id', 'stubIDENTIFIER_id');
+    expectExpr('id', 'stubIDENTIFIER_id');
   });
+
+
 
   // Binary expressions
-
   test('should parse binary expressions', () {
-    BVT.expectParse('3 + 4', '3 + 4');
+    expectExpr('3 + 4', '3 + 4');
   });
+
+
 
   // Cascade expressions
   test('should parse cascade expressions', () {
-    BVT.expectParse('x.foo()..boo()', 'CASCADES NOT IMPLEMENTED');
+    expectExpr('x.foo()..boo()', 'CASCADES NOT IMPLEMENTED');
   });
+
+
 
   // Conditional expressions
   test('should parse conditional expressions', () {
-    BVT.expectParse('true ? 4 : 3', 'true ? 4 : 3');
+    expectExpr('true ? 4 : 3', 'true ? 4 : 3');
   });
+
+
 
   // Function expressions
   test('should parse function expressions', () {
-    BVT.expectParse('x()', 'stubIDENTIFIER_x()');
+    expectExpr('x()', 'stubIDENTIFIER_x()');
   });
 
   test('should parse function expressions with parameters', () {
-    BVT.expectParse('x(1,2)', 'stubIDENTIFIER_x(1, 2)');
+    expectExpr('x(1,2)', 'stubIDENTIFIER_x(1, 2)');
   });
 
   test('should parse method expressions', () {
-    BVT.expectParse('b.x(1,2)',
+    expectExpr('b.x(1,2)',
       'stubIDENTIFIER_b.stubIDENTIFIER_x(1, 2)');
   });
 
+
+
   // Index expressions
   test('should parse index expressions', () {
-    BVT.expectParse('x[2]', 'stubIDENTIFIER_x[2]');
+    expectExpr('x[2]', 'stubIDENTIFIER_x[2]');
   });
+
+
 
   // Instance creatation expessions
   test('should parse a new call', () {
-    BVT.expectParse('new Y()', 'new stubIDENTIFIER_Y()');
+    expectExpr('new Y()', 'new stubIDENTIFIER_Y()');
   });
 
   test('should parse parans', () {
-    BVT.expectParse('4 * (2 + 2)', '4 * (2 + 2)');
+    expectExpr('4 * (2 + 2)', '4 * (2 + 2)');
   });
+
+
 
   // NOTE: the JS AST doesn't have parans and will remove
   // unneeded ones.
   test('should parse parans', () {
-    BVT.expectParse('(4) * ((2 + 2))', '4 * (2 + 2)');
+    expectExpr('(4) * ((2 + 2))', '4 * (2 + 2)');
   });
 
   test('should parse postfix expressions', () {
-    BVT.expectParse('i++', 'stubIDENTIFIER_i++');
+    expectExpr('i++', 'stubIDENTIFIER_i++');
   });
 
   test('should parse prefix expressions', () {
-    BVT.expectParse('++i', '++stubIDENTIFIER_i');
+    expectExpr('++i', '++stubIDENTIFIER_i');
   });
+
+
 
   // Property access
   test('should parse property access', () {
-    BVT.expectParse('a[4].b', 'stubIDENTIFIER_a[4].stubIDENTIFIER_b');
+    expectExpr('a[4].b', 'stubIDENTIFIER_a[4].stubIDENTIFIER_b');
   });
+
+
 
   // this
   test('should parse this', () {
-    BVT.expectParse('this', 'this');
+    expectExpr('this', 'this');
   });
+
+
 
   // throw
   // TODO(deboer): Investigate.  In JS, throw is a statement.
   //test('should parse throw', () {
-  //  BVT.expectParse('throw 5', 'throw 5');
+  //  expectExpr('throw 5', 'throw 5');
   //});
+
+
 
   // is expression
   test('should parse is', () {
-    BVT.expectParse('4 is int', 'typeof 4 == stubIDENTIFIER_int');
+    expectExpr('4 is int', 'typeof 4 == stubIDENTIFIER_int');
   });
 
   test('should parse is not', () {
-    BVT.expectParse('4 is ! int', 'typeof 4 != stubIDENTIFIER_int');
+    expectExpr('4 is ! int', 'typeof 4 != stubIDENTIFIER_int');
   });
+
+
 
   // assignments
   test('should parse assignments', () {
-    BVT.expectParse('f = 5', 'stubIDENTIFIER_f = 5');
+    expectExpr('f = 5', 'stubIDENTIFIER_f = 5');
   });
 
   test('should compound parse assignments', () {
-    BVT.expectParse('f *= 5', 'stubIDENTIFIER_f *= 5');
+    expectExpr('f *= 5', 'stubIDENTIFIER_f *= 5');
   });
 }
 
